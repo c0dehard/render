aa40f0a9dfc9ff2640d4619dc3a0a44349fd5b03
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-05-13 11:55:18 +0200

    Create render_test.go

---
+++ render_test.go
@@ -0,0 +1,23 @@
+package render
+
+import (
+	"bytes"
+	"strings"
+
+	"testing"
+)
+
+func TestWithByteBuffer(t *testing.T) {
+	out := new(bytes.Buffer)
+	Table(out, []string{"test", "version", "text"}, [][]string{{"1", "version 1", "empty"}, {"a2", "b2", "c3"}})
+
+	countedLines := strings.Count(out.String(), "\n")
+	if countedLines != 4 {
+		t.Fail()
+	}
+
+	fields := strings.Fields(out.String())
+	if fields[0] != "TEST" {
+		t.Fail()
+	}
+}
