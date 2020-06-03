337183cef16dd2428e165494bb0524b0c4a16489
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-06-03 14:32:37 +0200

    Using testify, is more simple. Ty @bkircher for this idea

--- render_test.go
+++ render_test.go
@@ -3,8 +3,9 @@ package render
 import (
 	"bytes"
 	"strings"
-
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWithByteBuffer(t *testing.T) {
@@ -12,12 +13,8 @@ func TestWithByteBuffer(t *testing.T) {
 	Table(out, []string{"test", "version", "text"}, [][]string{{"1", "version 1", "empty"}, {"a2", "b2", "c3"}})
 
 	countedLines := strings.Count(out.String(), "\n")
-	if countedLines != 4 {
-		t.Fail()
-	}
+	assert.Equal(t, countedLines, 3)
 
 	fields := strings.Fields(out.String())
-	if fields[0] != "TEST" {
-		t.Fail()
-	}
+	assert.Equal(t, fields[0], "TEST")
 }
